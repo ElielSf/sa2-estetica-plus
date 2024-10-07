f57df4252e40dd852edb0173257dc179c0eae0b2
Eliel Souza <elielsf13@gmail.com>
2024-10-07 09:26:59 -0300

    feat: adicionar testes de leitura, atualização e deleção

--- src/config/database.js
+++ src/config/database.js
@@ -14,6 +14,27 @@ export async function getAgendaById(id) {
 };
 
 export async function getAgendaByName(name) {
-    const [rows] = await connection.query("SELECT * FROM agenda WHERE nome_pessoa LIKE '%?%';", [name]);
+    const [rows] = await connection.query("SELECT * FROM agenda WHERE nome_pessoa = ?;", [name]);
+    return rows[0];
+}
+
+export async function getAgendaByNamePart(name) {
+    const [rows] = await connection.query("SELECT * FROM agenda WHERE nome_pessoa LIKE ?;", [`%${name}%`]);
+    return rows[0];
+}
+
+export async function getAgendaBetweenData() {
+    const [rows] = await connection.query("SELECT * FROM agenda WHERE data_agendamento BETWEEN '2024-10-02' AND '2024-10-10';");
+    return rows[0];
+}
+
+export async function updateAgendaById(id, new_date) {
+    await connection.query("UPDATE agenda SET data_agendamento = ? WHERE id_agenda = ?;", [new_date, id]);
+    const [rows] = await connection.query("SELECT * FROM agenda WHERE id_agenda = ?;", [id]);
+    return rows[0];
+};
+
+export async function deleteAgendaById(id) {
+    const [rows] = await connection.query("DELETE FROM agenda WHERE id_agenda = 1;", [id]);
     return rows[0];
 }
--- src/tests/database.test.js
+++ src/tests/database.test.js
@@ -1,15 +1,14 @@
-import { connection, getAgendaById, getAgendaByName } from "../config/database";
+import { connection, getAgendaBetweenData, getAgendaById, getAgendaByName, getAgendaByNamePart, updateAgendaById, deleteAgendaById } from "../config/database.js";
 
 describe("getAgendaById", () => {
 
     beforeAll(async () => {
-        await connection.query("CREATE TABLE IF NOT EXISTS agenda (`id_agenda` INT(11) NOT NULL,`nome_pessoa` VARCHAR(255) NOT NULL,`contato_telefonico` VARCHAR(13) NOT NULL,`email` VARCHAR(255) NOT NULL,`data_agendamento` VARCHAR(13) NOT NULL,PRIMARY KEY (`id_agenda`),UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)");
+        await connection.query("CREATE TABLE IF NOT EXISTS agenda (`id_agenda` INT(11) AUTO_INCREMENT NOT NULL,`nome_pessoa` VARCHAR(255) NOT NULL,`contato_telefonico` VARCHAR(13) NOT NULL,`email` VARCHAR(255) NOT NULL,`data_agendamento` VARCHAR(13) NOT NULL,PRIMARY KEY (`id_agenda`),UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)");
         await connection.query("INSERT INTO agenda (`nome_pessoa`, `contato_telefonico`, `email`, `data_agendamento`) VALUES ('Eliel Souza', '75933008953', 'eliel@gmail.com', '03/10/2024');");
     });
 
     afterAll(async () => {
         await connection.query("TRUNCATE TABLE agenda;");
-        await connection.end();
     });
 
     test("1 - Verificar se retorna o usuário correto", async () => {
@@ -19,11 +18,29 @@ describe("getAgendaById", () => {
         expect(agenda).toHaveProperty("nome_pessoa", "Eliel Souza");
         expect(agenda).toHaveProperty("contato_telefonico", "75933008953");
     });
+});
+
+describe("Leitura", () => {
+    beforeAll(async () => {
+        await connection.query("CREATE TABLE IF NOT EXISTS agenda (`id_agenda` INT(11) AUTO_INCREMENT NOT NULL,`nome_pessoa` VARCHAR(255) NOT NULL,`contato_telefonico` VARCHAR(13) NOT NULL,`email` VARCHAR(255) NOT NULL,`data_agendamento` VARCHAR(13) NOT NULL,PRIMARY KEY (`id_agenda`),UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)");
+        await connection.query("INSERT INTO agenda (`nome_pessoa`, `contato_telefonico`, `email`, `data_agendamento`) VALUES ('Eliel', '75933008953', 'eliel2@gmail.com', '2024-10-03');");
+        await connection.query("INSERT INTO agenda (`nome_pessoa`, `contato_telefonico`, `email`, `data_agendamento`) VALUES ('Eliel2', '75933008953', 'eliel3@gmail.com', '2024-10-07');");
+    });
+
+    afterAll(async () => {
+        await connection.query("TRUNCATE TABLE agenda;");
+    });
+
+    test("1 - Verificar se retorna o usuário por um nome específico", async () => {
+        const agendas = await getAgendaByName("Eliel");
+
+        expect(agendas.nome_pessoa).toMatch("Eliel");
+    });
 
     test("2 - Verificar se retorna o usuário por parte do nome", async () => {
-        const agendas = await getAgendaByName("Elie");
+        const agendas = await getAgendaByNamePart("Eli");
 
-        expect(agendas.nome_pessoa).toMatch(/Elie/);
+        expect(agendas.nome_pessoa).toMatch(/Eli/);
     });
 
     test("3 - Verificar se o tempo de resposta é menor que 100ms", async () => {
@@ -36,4 +53,44 @@ describe("getAgendaById", () => {
         expect(duracao).toBeLessThanOrEqual(100);
     });
 
+    test("4 - Obter os agendamentos entre 02/10/2024 e 10/10/2024", async () => {
+        const agendas = await getAgendaBetweenData();
+
+        expect(agendas).not.toBeUndefined();
+    });
+});
+
+describe("Atualizar", () => {
+    beforeAll(async () => {
+        await connection.query("CREATE TABLE IF NOT EXISTS agenda (`id_agenda` INT(11) AUTO_INCREMENT NOT NULL,`nome_pessoa` VARCHAR(255) NOT NULL,`contato_telefonico` VARCHAR(13) NOT NULL,`email` VARCHAR(255) NOT NULL,`data_agendamento` VARCHAR(13) NOT NULL,PRIMARY KEY (`id_agenda`),UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)");
+        await connection.query("INSERT INTO agenda (`nome_pessoa`, `contato_telefonico`, `email`, `data_agendamento`) VALUES ('Eliel', '75933008953', 'eliel2@gmail.com', '2024-10-03');");
+    });
+
+    afterAll(async () => {
+        await connection.query("TRUNCATE TABLE agenda;");
+    });
+
+    test("1 - Atualiza a data da agenda de um id específico", async () => {
+        const agenda_atualizada = await updateAgendaById(1, '2024-10-04');
+
+        expect(agenda_atualizada).toHaveProperty("data_agendamento", "2024-10-04");
+    });
+});
+
+describe("Deletar", () => {
+    beforeAll(async () => {
+        await connection.query("CREATE TABLE IF NOT EXISTS agenda (`id_agenda` INT(11) AUTO_INCREMENT NOT NULL,`nome_pessoa` VARCHAR(255) NOT NULL,`contato_telefonico` VARCHAR(13) NOT NULL,`email` VARCHAR(255) NOT NULL,`data_agendamento` VARCHAR(13) NOT NULL,PRIMARY KEY (`id_agenda`),UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)");
+        await connection.query("INSERT INTO agenda (`nome_pessoa`, `contato_telefonico`, `email`, `data_agendamento`) VALUES ('Eliel', '75933008953', 'eliel2@gmail.com', '2024-10-03');");
+    });
+
+    afterAll(async () => {
+    await connection.query("TRUNCATE TABLE agenda;");
+        await connection.end();
+    });
+
+    test("1 - Verificar se a agenda deletada pelo id é undefined", async () => {
+        const agenda_deletada = await deleteAgendaById(1);
+
+        expect(agenda_deletada).toBeUndefined();
+    });
 });
